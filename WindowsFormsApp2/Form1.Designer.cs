f32991af565dd54cde57f86da26a1a146a1e0a7c
zengyunsi <zengyunsi@DESKTOP-RI60OA4>
2024-08-13 04:15:43 +0800

    添加项目文件。

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace MyChart
+{
+    partial class 图表测试demo
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.时钟 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.选择框 = new System.Windows.Forms.CheckBox();
+            this.查询 = new System.Windows.Forms.Button();
+            this.修改 = new System.Windows.Forms.Button();
+            this.生成 = new System.Windows.Forms.Button();
+            this.删除 = new System.Windows.Forms.Button();
+            this.排序 = new System.Windows.Forms.Button();
+            this.插入 = new System.Windows.Forms.Button();
+            this.数值 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.位置 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.样式 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "操作位置:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // 时钟
+            // 
+            this.时钟.Interval = 1000;
+            this.时钟.Tick += new System.EventHandler(this.时钟_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.样式);
+            this.panel1.Controls.Add(this.选择框);
+            this.panel1.Controls.Add(this.查询);
+            this.panel1.Controls.Add(this.修改);
+            this.panel1.Controls.Add(this.生成);
+            this.panel1.Controls.Add(this.删除);
+            this.panel1.Controls.Add(this.排序);
+            this.panel1.Controls.Add(this.插入);
+            this.panel1.Controls.Add(this.数值);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.位置);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1329, 113);
+            this.panel1.TabIndex = 3;
+            // 
+            // 选择框
+            // 
+            this.选择框.AutoSize = true;
+            this.选择框.Location = new System.Drawing.Point(1077, 81);
+            this.选择框.Name = "选择框";
+            this.选择框.Size = new System.Drawing.Size(232, 22);
+            this.选择框.TabIndex = 11;
+            this.选择框.Text = "自动生成(和上面不通用)";
+            this.选择框.UseVisualStyleBackColor = true;
+            this.选择框.CheckedChanged += new System.EventHandler(this.选择框_CheckedChanged);
+            // 
+            // 查询
+            // 
+            this.查询.Location = new System.Drawing.Point(733, 25);
+            this.查询.Name = "查询";
+            this.查询.Size = new System.Drawing.Size(100, 50);
+            this.查询.TabIndex = 10;
+            this.查询.Text = "查询一行";
+            this.查询.UseVisualStyleBackColor = true;
+            this.查询.Click += new System.EventHandler(this.查询_Click);
+            // 
+            // 修改
+            // 
+            this.修改.Location = new System.Drawing.Point(852, 25);
+            this.修改.Name = "修改";
+            this.修改.Size = new System.Drawing.Size(100, 50);
+            this.修改.TabIndex = 9;
+            this.修改.Text = "修改一行";
+            this.修改.UseVisualStyleBackColor = true;
+            this.修改.Click += new System.EventHandler(this.修改_Click);
+            // 
+            // 生成
+            // 
+            this.生成.Location = new System.Drawing.Point(1209, 25);
+            this.生成.Name = "生成";
+            this.生成.Size = new System.Drawing.Size(100, 50);
+            this.生成.TabIndex = 8;
+            this.生成.Text = "生成数据";
+            this.生成.UseVisualStyleBackColor = true;
+            this.生成.Click += new System.EventHandler(this.生成_Click);
+            // 
+            // 删除
+            // 
+            this.删除.Location = new System.Drawing.Point(614, 25);
+            this.删除.Name = "删除";
+            this.删除.Size = new System.Drawing.Size(100, 50);
+            this.删除.TabIndex = 7;
+            this.删除.Text = "删除一行";
+            this.删除.UseVisualStyleBackColor = true;
+            this.删除.Click += new System.EventHandler(this.删除_Click);
+            // 
+            // 排序
+            // 
+            this.排序.Location = new System.Drawing.Point(971, 25);
+            this.排序.Name = "排序";
+            this.排序.Size = new System.Drawing.Size(100, 50);
+            this.排序.TabIndex = 6;
+            this.排序.Text = "数据排序";
+            this.排序.UseVisualStyleBackColor = true;
+            this.排序.Click += new System.EventHandler(this.排序_Click);
+            // 
+            // 插入
+            // 
+            this.插入.Location = new System.Drawing.Point(495, 25);
+            this.插入.Name = "插入";
+            this.插入.Size = new System.Drawing.Size(100, 50);
+            this.插入.TabIndex = 5;
+            this.插入.Text = "随意插入";
+            this.插入.UseVisualStyleBackColor = true;
+            this.插入.Click += new System.EventHandler(this.插入_Click);
+            // 
+            // 数值
+            // 
+            this.数值.Location = new System.Drawing.Point(132, 59);
+            this.数值.Name = "数值";
+            this.数值.Size = new System.Drawing.Size(100, 28);
+            this.数值.TabIndex = 4;
+            this.数值.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "插入数值:";
+            // 
+            // 位置
+            // 
+            this.位置.Location = new System.Drawing.Point(132, 25);
+            this.位置.Name = "位置";
+            this.位置.Size = new System.Drawing.Size(100, 28);
+            this.位置.TabIndex = 2;
+            this.位置.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "末尾添加";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.chart1);
+            this.panel2.Location = new System.Drawing.Point(-2, 120);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1329, 976);
+            this.panel2.TabIndex = 4;
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Black;
+            this.chart1.BorderlineColor = System.Drawing.Color.Black;
+            chartArea4.AxisX.LabelStyle.ForeColor = System.Drawing.Color.LimeGreen;
+            chartArea4.AxisX.LineColor = System.Drawing.Color.LimeGreen;
+            chartArea4.AxisX.MajorGrid.Interval = 0D;
+            chartArea4.AxisX.MajorGrid.IntervalOffset = 0D;
+            chartArea4.AxisX.MajorGrid.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Auto;
+            chartArea4.AxisX.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(62)))), ((int)(((byte)(4)))));
+            chartArea4.AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea4.AxisX.MajorGrid.LineWidth = 2;
+            chartArea4.AxisX.MajorTickMark.Enabled = false;
+            chartArea4.AxisX.MajorTickMark.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea4.AxisX.MajorTickMark.LineColor = System.Drawing.Color.LimeGreen;
+            chartArea4.AxisX.MaximumAutoSize = 50F;
+            chartArea4.AxisX.MinorGrid.LineColor = System.Drawing.Color.LimeGreen;
+            chartArea4.AxisX.MinorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            chartArea4.AxisX.MinorTickMark.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            chartArea4.AxisX.MinorTickMark.TickMarkStyle = System.Windows.Forms.DataVisualization.Charting.TickMarkStyle.None;
+            chartArea4.AxisX.ScaleBreakStyle.LineColor = System.Drawing.Color.Tomato;
+            chartArea4.AxisX.Title = "横坐标";
+            chartArea4.AxisX.TitleAlignment = System.Drawing.StringAlignment.Far;
+            chartArea4.AxisX.TitleForeColor = System.Drawing.Color.Chartreuse;
+            chartArea4.AxisX.ToolTip = "2131";
+            chartArea4.AxisX2.MajorGrid.Enabled = false;
+            chartArea4.AxisX2.Title = "123";
+            chartArea4.AxisY.LabelStyle.ForeColor = System.Drawing.Color.LimeGreen;
+            chartArea4.AxisY.LineColor = System.Drawing.Color.LimeGreen;
+            chartArea4.AxisY.MajorGrid.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Auto;
+            chartArea4.AxisY.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(62)))), ((int)(((byte)(4)))));
+            chartArea4.AxisY.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea4.AxisY.MajorGrid.LineWidth = 2;
+            chartArea4.AxisY.MajorTickMark.Enabled = false;
+            chartArea4.AxisY.MajorTickMark.LineColor = System.Drawing.Color.LimeGreen;
+            chartArea4.AxisY.MinorGrid.LineColor = System.Drawing.Color.LimeGreen;
+            chartArea4.AxisY.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Stacked;
+            chartArea4.AxisY.Title = "纵坐标";
+            chartArea4.AxisY.TitleAlignment = System.Drawing.StringAlignment.Far;
+            chartArea4.AxisY.TitleForeColor = System.Drawing.Color.LimeGreen;
+            chartArea4.AxisY2.Title = "321";
+            chartArea4.BackColor = System.Drawing.Color.Black;
+            chartArea4.CursorX.SelectionColor = System.Drawing.Color.Transparent;
+            chartArea4.CursorY.SelectionColor = System.Drawing.Color.Transparent;
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend4.Enabled = false;
+            legend4.HeaderSeparatorColor = System.Drawing.Color.LimeGreen;
+            legend4.ItemColumnSeparatorColor = System.Drawing.Color.LimeGreen;
+            legend4.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Row;
+            legend4.Name = "Legend1";
+            legend4.TableStyle = System.Windows.Forms.DataVisualization.Charting.LegendTableStyle.Tall;
+            legend4.TitleForeColor = System.Drawing.Color.LimeGreen;
+            legend4.TitleSeparatorColor = System.Drawing.Color.LimeGreen;
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series4.BackHatchStyle = System.Windows.Forms.DataVisualization.Charting.ChartHatchStyle.BackwardDiagonal;
+            series4.BackImage = "LimeGreen";
+            series4.BackImageTransparentColor = System.Drawing.Color.LimeGreen;
+            series4.BackSecondaryColor = System.Drawing.Color.LimeGreen;
+            series4.BorderColor = System.Drawing.Color.LimeGreen;
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Color = System.Drawing.Color.LimeGreen;
+            series4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series4.LabelBackColor = System.Drawing.Color.Transparent;
+            series4.LabelBorderColor = System.Drawing.Color.Transparent;
+            series4.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            series4.LabelForeColor = System.Drawing.Color.Yellow;
+            series4.Legend = "Legend1";
+            series4.LegendText = "电压";
+            series4.LegendToolTip = "电压变化";
+            series4.MarkerColor = System.Drawing.Color.Red;
+            series4.Name = "电压";
+            series4.YValuesPerPoint = 6;
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(1329, 976);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "测试";
+            this.chart1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseMove);
+            // 
+            // 样式
+            // 
+            this.样式.Location = new System.Drawing.Point(1090, 25);
+            this.样式.Name = "样式";
+            this.样式.Size = new System.Drawing.Size(100, 50);
+            this.样式.TabIndex = 12;
+            this.样式.Text = "换个样式";
+            this.样式.UseVisualStyleBackColor = true;
+            this.样式.Click += new System.EventHandler(this.样式_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(257, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 50);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "清空数据";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // 图表测试demo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1326, 1096);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "图表测试demo";
+            this.Text = "图表测试demo";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.图表测试demo_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer 时钟;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox 位置;
+        private System.Windows.Forms.TextBox 数值;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button 排序;
+        private System.Windows.Forms.Button 插入;
+        private System.Windows.Forms.Button 删除;
+        private System.Windows.Forms.Button 生成;
+        private System.Windows.Forms.Button 修改;
+        private System.Windows.Forms.Button 查询;
+        private System.Windows.Forms.CheckBox 选择框;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button 样式;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MyChart
+{
+    public partial class 图表测试demo : Form
+    {
+
+        // 生成数据用
+        int[] a = new int[50];
+        
+        // 计数用
+        int count = 0;
+
+
+        // 自动生成随机用
+        private int 循环数;
+
+        Random rnd = new Random();//生成随机数种子
+
+        // 在类中定义一个字段来存储所有的图表类型
+        private List<SeriesChartType> chartTypes;
+        private int currentChartTypeIndex = 0;
+
+
+
+
+
+        public 图表测试demo()
+        {
+            InitializeComponent();
+
+            //这段代码的作用是初始化一个整型数组和一个计数器变量，具体解释如下：
+            //这两个变量配合使用，数组 a 用来存储数据，而 count 用来记录已经存储了多少数据。这样在后续的代码中，可以利用 count 来控制数据的添加和处理。
+
+            // 初始化图表类型列表
+            chartTypes = new List<SeriesChartType>
+    {
+
+             
+        SeriesChartType.Line,
+        SeriesChartType.Spline,
+        SeriesChartType.StepLine,
+        SeriesChartType.FastLine,
+        SeriesChartType.Area,
+        SeriesChartType.SplineArea,
+        SeriesChartType.StackedArea,
+        SeriesChartType.StackedArea100,
+        SeriesChartType.Stock,
+        SeriesChartType.Range,
+        SeriesChartType.SplineRange,
+        SeriesChartType.Polar,
+        SeriesChartType.ErrorBar,
+        SeriesChartType.Kagi,
+        SeriesChartType.PointAndFigure,
+        SeriesChartType.Pie,
+        SeriesChartType.Doughnut
+        // 添加其他你想要的图表类型
+    };
+
+
+
+            循环数 = 0;
+
+            chart1.Series[0].Points.Clear();
+
+
+
+
+            //鼠标停留显示数值
+            //chart1.Series[0].ToolTip = "X: #VALX, Y: #VALY";
+
+
+            //chart1.Series[0].Points.AddXY(1, 1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create cursor object
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//添加
+        {
+
+            // 这段代码是在一个按钮点击事件（`button1_Click`）中执行的。它的作用是将文本框 `textBox2` 中的文本转换为整数，并将其存储在一个数组 `a` 的指定位置（由 `count` 索引指定），然后递增 `count`，最后调用 `updateChart()` 函数更新图表。具体解释如下：
+ 
+            a[count] = Convert.ToInt32(数值.Text);
+
+            count++;
+
+
+            更新图表();
+        }
+        void 更新图表()//重新绘制
+        {
+
+            //这一行代码清空图表中 Series[0] 的所有数据点。这样做是为了确保在重新绘制图表之前，先移除旧的数据。
+            //这是一段循环代码，从 i = 0 开始循环，一直到 i < count 为止，每次循环 i 递增1。count 是一个表示当前数据点数量的变量。
+            //在每次循环中，这一行代码将 i 和数组 a 中对应索引 i 的值作为X和Y轴的值，添加到图表的 Series[0] 中。这样就把数据绘制到了图表上。
+             
+            chart1.Series[0].Points.Clear();
+
+            for (int i = 0; i < count; i++)
+
+                chart1.Series[0].Points.AddXY(i, a[i]);
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 插入_Click(object sender, EventArgs e)//插入数据
+        {
+
+            int pos = Convert.ToInt32(位置.Text);
+
+            if (pos >= count) pos = count;
+
+            for (int i = count; i > pos; i--)
+
+                a[i] = a[i - 1];
+
+            a[pos] = Convert.ToInt32(位置.Text); //插入数据
+
+            count++;
+
+            更新图表();
+        }
+
+        private void 删除_Click(object sender, EventArgs e)
+        {
+            int pos = Convert.ToInt32(位置.Text);
+            //将 textBox1 中的文本转换为整数，并存储在变量 pos 中，表示要删除的数据的位置。
+            if (pos > count - 1 || pos < 0)
+            //这段代码检查 pos 是否在有效范围内。如果 pos 超出了当前数组中的数据范围（即 pos 大于 count - 1 或小于 0），则执行以下代码。
+            {
+                System.Media.SystemSounds.Beep.Play();
+                //播放一个系统提示音，提示用户输入的位置无效。
+                return;
+                //return;
+            }
+            for (int i = pos; i <= count - 2; i++) //删除数据
+                //这段循环代码从 pos 开始，将 pos 位置之后的数据依次向前移动一位，以覆盖被删除的数据。
+                a[i] = a[i + 1];
+            //这一行在循环中执行，将数组中 i + 1 位置的值赋给 i 位置，实现数据的前移。
+            count--;
+            //删除数据后，count 减少 1，表示数组中的数据数量减少。
+
+            更新图表();
+
+
+
+
+        }
+
+        private void 生成_Click(object sender, EventArgs e)//生成
+        {
+
+            Random rnd = new Random(); // 生成随机数的对象
+
+            count = 50; //图表数据修改为50个
+
+            for (int i = 0; i < 50; i++)
+            {
+                a[i] = rnd.Next(1, 36); // 生成1到255之间的随机数
+            }
+
+            更新图表();
+        }
+
+        private void 排序_Click(object sender, EventArgs e)//排序
+        {
+
+            List<int> listValues = new List<int>(); // 创建一个新的整型列表
+            listValues.Clear(); // 清空列表中的所有元素
+
+            for (int i = 0; i < count; i++) // 将数组a中的元素添加到列表中
+                listValues.Add(a[i]);
+
+            listValues.Sort(); // 对列表中的元素进行升序排序
+                               // listValues.Reverse(); // 如果需要逆序排列，可以取消注释这行代码
+
+            for (int i = 0; i < count; i++) // 将排序后的列表中的元素重新放回数组a中
+                a[i] = listValues[i];
+
+
+
+            更新图表();
+        }
+
+        private void 修改_Click(object sender, EventArgs e)//修改
+        {
+            int pos = Convert.ToInt32(位置.Text); // 将textBox1中的文本转换为整数，表示要修改的位置
+            if (pos > count - 1 || pos < 0) // 检查位置是否在有效范围内
+            {
+                System.Media.SystemSounds.Beep.Play(); // 如果位置无效，播放提示音
+                return; // 退出方法，不执行后续代码
+            }
+            a[pos] = Convert.ToInt32(数值.Text); // 修改数组a中指定位置的值，将其设为textBox2中的值
+
+
+            更新图表();
+        }
+
+        private void 查询_Click(object sender, EventArgs e)
+        {
+            int pos = Convert.ToInt32(位置.Text); // 将textBox1中的文本转换为整数，表示要查询的位置
+            if (pos > count - 1 || pos < 0) // 检查位置是否在有效范围内
+            {
+                System.Media.SystemSounds.Beep.Play(); // 如果位置无效，播放提示音
+                return; // 退出方法，不执行后续代码
+            }
+
+
+            MessageBox.Show(a[pos].ToString());
+
+            更新图表();
+
+        }
+
+
+
+        private void 时钟_Tick(object sender, EventArgs e)
+        {
+ 
+
+            int yValue = rnd.Next(3, 25);
+
+
+            chart1.Series[0].Points.AddXY(循环数, yValue);
+
+
+            // 假设你要将X轴标题更新为当前时间的字符串表示
+
+            string 电压 = yValue.ToString();
+
+            DateTime currentValue = DateTime.Now;
+
+            // 更新X轴的标题为当前值
+            chart1.ChartAreas[0].AxisX.Title = $"电压: {电压} V 时间: {currentValue:HH:mm:ss}";
+
+
+            // 隐藏所有数据点的标签
+            foreach (var point in chart1.Series[0].Points)
+
+            {
+                point.Label = string.Empty;
+            }
+
+            // 显示最新数据点的标签
+            chart1.Series[0].Points[chart1.Series[0].Points.Count - 1].Label = yValue.ToString() + " V";
+
+            // 使X轴滚动，以显示最新的20个数据点
+            if (chart1.Series[0].Points.Count > 20)
+
+            {
+                chart1.ChartAreas[0].AxisX.Minimum = 循环数 - 19;
+                chart1.ChartAreas[0].AxisX.Maximum = 循环数;
+            }
+
+            循环数++;
+
+        }
+
+
+
+        private void 选择框_CheckedChanged(object sender, EventArgs e)
+        {
+            if (选择框.Checked)
+            {
+
+                chart1.Series[0].Points.Clear();
+
+                时钟.Interval = 200;
+
+                // 重置 X 轴的值为默认值（自动计算）
+                chart1.ChartAreas[0].AxisX.Minimum = Double.NaN;
+
+                // 重置 X 轴的最大值为默认值
+                chart1.ChartAreas[0].AxisX.Maximum = Double.NaN;
+                chart1.Invalidate();
+
+
+                循环数 = 0;
+
+                时钟.Start();
+
+                Debug.WriteLine("时钟开启了!");
+                // 在这里添加你想要执行的逻辑
+            }
+            else
+            {
+
+                时钟.Stop();
+
+                Debug.WriteLine("时钟关闭了!");
+                // 在这里添加取消选中时的逻辑
+            }
+
+        }//随机输入数据
+
+        private void chart1_MouseMove(object sender, MouseEventArgs e)//实时显示数据
+        {
+
+            // 将鼠标坐标转换为图表区域内的坐标
+            HitTestResult result = chart1.HitTest(e.X, e.Y);
+
+            // 检查鼠标是否位于数据点上
+            if (result.ChartElementType == ChartElementType.DataPoint)
+            {
+                DataPoint point = chart1.Series[0].Points[result.PointIndex];
+
+                string toolTipText = $"时间: {point.XValue}, 电压: {point.YValues[0]} V";
+
+
+                // 显示自定义的ToolTip
+                toolTip1.Show(toolTipText, chart1, e.X, e.Y - 15);
+
+            }
+            else
+            {
+                // 如果鼠标没有悬停在数据点上，隐藏ToolTip
+                toolTip1.Hide(chart1);
+            }
+
+
+
+        }
+
+        private void 样式_Click(object sender, EventArgs e)//样式切换
+        {
+            // 切换到下一个图表类型
+            currentChartTypeIndex++;
+            if (currentChartTypeIndex >= chartTypes.Count)
+            {
+                currentChartTypeIndex = 0; // 回到第一个类型
+            }
+
+            // 设置当前的图表类型
+            chart1.Series[0].ChartType = chartTypes[currentChartTypeIndex];
+        }
+
+        private void 图表测试demo_FormClosed(object sender, FormClosedEventArgs e)//安全销毁
+        {
+            chart1.Series[0].Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)//清空
+        {
+
+            时钟.Stop();
+
+            chart1.Series[0].Points.Clear();
+
+            // 重置 X 轴的值为默认值（自动计算）
+            chart1.ChartAreas[0].AxisX.Minimum = Double.NaN;
+
+            // 重置 X 轴的最大值为默认值
+            chart1.ChartAreas[0].AxisX.Maximum = Double.NaN;
+
+            chart1.Invalidate();
+
+            循环数 = 0;
+
+        }
+    }
+}
